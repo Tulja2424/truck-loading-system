f09267e9d88728492fe864a5c97d56c5e889894c
Tulja2424 <tuljabirla@gmail.com>
2026-03-18 01:36:21 +0530

    updated project

--- models/dealer.js
+++ models/dealer.js
@@ -1,7 +1,10 @@
 const mongoose=require('mongoose');
 // mongoose.connect('mongodb://127.0.0.1:27017/truck-loading-system');
-mongoose.connect("mongodb+srv://birlatulja_db_user:Uzr0cl6DAbkUcnLv@cluster0.88az9km.mongodb.net/truckSystem")
-.then(()=> console.log("MongoDB Connected")).catch(err => console.log(err));
+//mongoose.connect("mongodb+srv://birlatulja_db_user:Uzr0cl6DAbkUcnLv@cluster0.88az9km.mongodb.net/truckSystem")
+//.then(()=> console.log("MongoDB Connected")).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 const dealerSchema=mongoose.Schema({
     name:String,
     password:String,
